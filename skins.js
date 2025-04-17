b6a3c8c9f2f0a942611ed6caa56e0972cd6f6112
Gamer Arnab XYZ <arnabdasdas2010@gmail.com>
2025-04-17 12:13:18 +0600

    Add files via upload

---
+++ skins.js
@@ -0,0 +1,27 @@
+// skins.js
+const skins = [
+  {
+    id: "steve",
+    name: "Steve",
+    image: "images/steve.png",
+    cost: 0
+  },
+  {
+    id: "alex",
+    name: "Alex",
+    image: "images/alex.png",
+    cost: 0
+  },
+  {
+    id: "miner",
+    name: "Miner",
+    image: "images/miner.png",
+    cost: 20
+  },
+  {
+    id: "zombie",
+    name: "Zombie",
+    image: "images/zombie.png",
+    cost: 50
+  }
+];
